f1e3503021bd37881f41e334c66cf71cfd5e0867
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-01-20 19:41:30 -0500

    Update ForoHubApplication.java

--- src/main/java/com/Foro_Hub/Alura/Foro_Hub/ForoHubApplication.java
+++ src/main/java/com/Foro_Hub/Alura/Foro_Hub/ForoHubApplication.java
@@ -1,14 +1,24 @@
 package com.Foro_Hub.Alura.Foro_Hub;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @SpringBootApplication
-public class ForoHubApplication {
+public class ForoHubApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
