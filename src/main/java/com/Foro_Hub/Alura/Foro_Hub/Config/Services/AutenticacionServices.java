14f799a53822d0c1b3e7cf81fbedebb8522fee58
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-01-20 20:20:00 -0500

    Update All
    Update Services y pom

--- src/main/java/com/Foro_Hub/Alura/Foro_Hub/Config/Services/AutenticacionServices.java
+++ src/main/java/com/Foro_Hub/Alura/Foro_Hub/Config/Services/AutenticacionServices.java
@@ -2,8 +2,7 @@ package com.Foro_Hub.Alura.Foro_Hub.Config.Services;
 
 import com.Foro_Hub.Alura.Foro_Hub.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
